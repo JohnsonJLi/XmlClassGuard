4fb502a23186511b10a125917d90848f4cf995c6
yuanquan <yuanquan@yizhoucp.com>
2024-04-25 20:19:06 +0800

    run

--- plugin/src/main/java/com/xml/guard/tasks/CompressAndResizeTask.kt
+++ plugin/src/main/java/com/xml/guard/tasks/CompressAndResizeTask.kt
@@ -6,6 +6,7 @@ import com.xml.guard.utils.resDir
 import net.coobird.thumbnailator.Thumbnails
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
+import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardCopyOption
@@ -44,16 +45,16 @@ open class CompressAndResizeTask @Inject constructor(
                     }
                     .forEach { insertDir ->
                         println("XmlInsertTask:> compressDir : ${insertDir} >f  ${insertDir.fileName}")
-                        insertDir.toFile().listFiles { file -> file.isFile && (file.extension == "jpg" || file.extension == "png" /*|| file.extension == "webp"*/) }?.forEach { inputFile ->
-                            val tempFilePath = inputFile.resolveSibling("${inputFile.name}.temp").toPath()
+                        insertDir.toFile().listFiles { file -> file.isFile && (file.extension == "jpg" || file.extension == "png" || file.extension == "webp") }?.forEach { inputFile ->
+                            val tempFilePath = File("${inputFile.parent}/${inputFile.name}_temp.${inputFile.extension}")
 
                             Thumbnails.of(inputFile.absolutePath)
-                                .scale(0.5) // 缩小图片尺寸到原尺寸的50%
+                                .scale(0.999) // 缩小图片尺寸到原尺寸的50%
                                 .outputQuality(0.98) // 设置压缩质量为80%
-                                .toFile(tempFilePath.toFile())
+                                .toFile(tempFilePath)
 
                             println("Processed image: ${inputFile.name}")
-                            Files.move(tempFilePath, inputFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+                            Files.move(tempFilePath.toPath(), inputFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
                         }
                     }
             } catch (e: Exception) {
